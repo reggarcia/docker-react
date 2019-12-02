532aeacf4b96b6ac55af1d796dcf4655ad13e416
Regina Garcia <regina_garcia@intuit.com>
2019-12-01 20:26:02 -0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         hello docker react
+         hello docker react 123
         </p>
         <a
           className="App-link"
